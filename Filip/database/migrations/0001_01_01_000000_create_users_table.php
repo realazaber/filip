33d72e69da1a265fb9f6540d3537097db5d4c048
AZ <47402666+realazaber@users.noreply.github.com>
2025-11-13 13:41:59 +0100

    no message

--- Filip/database/migrations/0001_01_01_000000_create_users_table.php
+++ Filip/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth')->nullable();
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->boolean('filament_user')->default(false);
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('gender', ['Not Specified', 'Male', 'Female'])->default('Not Specified');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
